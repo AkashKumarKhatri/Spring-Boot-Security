91f7479e232b3a7e1f442d29c12d3548933722b9
Akash <kkatriakash@gmail.com>
2019-08-20 22:26:16 +0500

    Adding Spring Security to new Spring Boot project.

---
+++ Spring-Boot-Security-Level-1/src/main/java/com/sapphire/security/SpringBootSecurityLevel1Application.java
@@ -0,0 +1,13 @@
+package com.sapphire.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityLevel1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityLevel1Application.class, args);
+	}
+
+}
