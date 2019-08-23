bc36b89892dcf34ef17ece8ddf2f0e9aeb8f4a7e
Akash <kkatriakash@gmail.com>
2019-08-23 20:14:58 +0500

    How to configure Spring Security Authorization.

--- Spring-Boot-Security-Level-3/src/test/java/com/sapphire/security/SpringBootSecurityLevel3ApplicationTests.java
+++ Spring-Boot-Security-Level-3/src/test/java/com/sapphire/security/SpringBootSecurityLevel3ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootSecurityLevel2ApplicationTests {
+public class SpringBootSecurityLevel3ApplicationTests {
 
 	@Test
 	public void contextLoads() {
