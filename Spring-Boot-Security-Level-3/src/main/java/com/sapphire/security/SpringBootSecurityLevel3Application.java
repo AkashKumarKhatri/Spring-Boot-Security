561cbf4a86d7b9b1b443ab61f9d89cd2810123d2
Akash <kkatriakash@gmail.com>
2019-08-23 20:05:40 +0500

    How to configure Spring Security Authorization.

---
+++ Spring-Boot-Security-Level-3/src/main/java/com/sapphire/security/SpringBootSecurityLevel3Application.java
@@ -0,0 +1,13 @@
+package com.sapphire.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityLevel3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityLevel3Application.class, args);
+	}
+
+}
